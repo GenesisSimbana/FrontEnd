76ce111b2593a9ba0c31723d97024038bd7c62b9
GiovannaSoria <gmsoria@espe.edu.ec>
2025-07-11 10:29:08 -0500

    env variables update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    cors: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['ec2-18-116-67-249.us-east-2.compute.amazonaws.com'],
+  }
 })
+
